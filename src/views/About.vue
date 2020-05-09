8a6e3982e9d204b62af2a37a6f8833d02e47ab21
Douglas Schonholtz <schonholtzd@gmail.com>
2020-05-09 14:55:12 -0400

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <h3 class="author-tag">
+      By: Douglas Schonholtz
+    </h3>
+    <div class="about-info">
+      <p class="about-info__paragraph">
+          For my mom Marie Schonholtz.
+          This website is built using VueJS 3.0.
+          SCSS, and TypeScript.
+      </p>
+    </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Under construction!"/>
+    <p class="hello__mom">
+      Happy Mothers Day Mom!
+      I hope it is a good one :)
+      I love you lots and you mean the world to me.
+    </p>
   </div>
 </template>
 
